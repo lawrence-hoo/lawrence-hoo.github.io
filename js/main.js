78c713751147c002e4bc99f0df4f551b31024ad8
Lawrence Hoo <law.a.hoo@gmail.com>
2015-12-08 14:53:34 -0500

    animations use js instead of css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 $( document ).ready( function() {
+
+    var win_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    $( window ).resize( function() {
+        win_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    });
+
     $( '.position' ).each( function() {
         var position = this;
-        var position_content = $( position ).find( '.position-content' );
-
+        var position_content_container = $( position ).children( '.position-content-container' );
+        var position_content = $( position_content_container ).children( '.position-content' );
+        
         $( position ).find( '.gray-indent-plus' ).click( function() {
-            $( position ).toggleClass( 'is-open' );
+            if ( win_width < 640) {
+                if ( $( position ).hasClass( 'is-open' ) ) {
+                    $( position_content ).animate( {
+                        'opacity': '0'
+                    }, 600 );
+                    $( position_content_container ).delay( 600 ).slideToggle(600);
+                }
+                else {
+                    $( position_content_container ).slideToggle(600);
+                    $( position_content ).delay( 600 ).animate( {
+                        'opacity': '1'
+                    }, 600 );
+                }
+                $( position ).toggleClass( 'is-open' );
+            }
         });
     });
 });
